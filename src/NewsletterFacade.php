b8a001e2f6e9fb845afbc7a771d826c896bacf99
Gaël Reyrol <gael@dansmaculotte.fr>
2020-02-20 15:25:51 +0100

    fix: add php doc methods to facade

--- src/NewsletterFacade.php
+++ src/NewsletterFacade.php
@@ -4,6 +4,18 @@ namespace DansMaCulotte\Newsletter;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see Newsletter
+ * @method static subscribe(string $email, array $options = [], string $listName = '')
+ * @method static subscribeOrUpdate(string $email, array $options = [], string $listName = '')
+ * @method static getMembers(string $listName, array $parameters = [])
+ * @method static getMember(string $email, string $listName = '')
+ * @method static bool hasMember(string $email, string $listName = '')
+ * @method static bool isSubscribed(string $email, string $listName = '')
+ * @method static unsubscribe(string $email, string $listName = '')
+ * @method static delete(string $email, string $listName = '')
+ * @method static getApi()
+ */
 class NewsletterFacade extends Facade
 {
     public static function getFacadeAccessor()
